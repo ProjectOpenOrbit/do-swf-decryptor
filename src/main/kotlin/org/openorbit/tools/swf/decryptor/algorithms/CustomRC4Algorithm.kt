ce5267b7d4c199aa1cae98bcb521f6421974adbc
tram98 <jannickoursin1@gmail.com>
2023-01-31 23:25:22 +0100

    implemented RC4 decryptor

--- src/main/kotlin/org/openorbit/tools/swf/decryptor/algorithms/CustomRC4Algorithm.kt
+++ src/main/kotlin/org/openorbit/tools/swf/decryptor/algorithms/CustomRC4Algorithm.kt
@@ -1,4 +1,34 @@
 package org.openorbit.tools.swf.decryptor.algorithms
 
-class CustomRC4Algorithm {
+import org.openorbit.tools.common.utils.byteArrayFromHexStr
+import java.security.MessageDigest
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+private const val ALGORITHM_RC4 = "RC4"
+
+class CustomRC4Algorithm(rawKeyStringFromLoadingscreen: String) {
+
+    private val maxLengthOfHashedKey = 16
+    private lateinit var hashedKey: ByteArray
+
+    init {
+        hashKey(byteArrayFromHexStr(rawKeyStringFromLoadingscreen))
+    }
+
+    private fun hashKey(key: ByteArray) {
+        val fullHash = MessageDigest.getInstance("SHA-256").digest(key)
+        val keyLen = minOf(maxLengthOfHashedKey, fullHash.size)
+        hashedKey = ByteArray(keyLen)
+        for (idx in 0 until keyLen) {
+            hashedKey[idx] = fullHash[idx]
+        }
+    }
+
+    fun decrypt(data: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance(ALGORITHM_RC4)
+        val key = SecretKeySpec(hashedKey, ALGORITHM_RC4)
+        cipher.init(Cipher.DECRYPT_MODE, key)
+        return cipher.doFinal(data)
+    }
 }
