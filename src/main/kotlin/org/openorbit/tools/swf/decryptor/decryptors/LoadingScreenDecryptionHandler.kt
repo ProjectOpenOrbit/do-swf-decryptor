946f814887ab2bb447530f65e6e95a0cfde7207f
tram98 <jannickoursin1@gmail.com>
2023-01-31 21:57:53 +0100

    Fixed issue with loadingscreen decryptor, file must be decompressed first.

--- src/main/kotlin/org/openorbit/tools/swf/decryptor/decryptors/LoadingScreenDecryptionHandler.kt
+++ src/main/kotlin/org/openorbit/tools/swf/decryptor/decryptors/LoadingScreenDecryptionHandler.kt
@@ -1,15 +1,24 @@
 package org.openorbit.tools.swf.decryptor.decryptors
 
-import kotlin.experimental.xor
+import java.util.zip.Inflater
+import java.util.zip.InflaterInputStream
 
 object LoadingScreenDecryptionHandler {
     fun decryptLoadingScreen(encryptedData: ByteArray): ByteArray {
+        val decompressedData = encryptedData.inflated()
+
         var xorByte = 57
-        val decryptedData = ByteArray(encryptedData.size)
-        encryptedData.forEachIndexed { i, curByte ->
-            xorByte = (xorByte + 3) and 255
-            decryptedData[i] = curByte.xor(xorByte.toByte())
+        decompressedData.forEachIndexed { i, curByte ->
+            xorByte = (xorByte + 3) % 256
+            decompressedData[i] = curByte.toInt().xor(xorByte).toByte()
         }
-        return decryptedData
+        return decompressedData
+    }
+
+    /**
+     * extension function to easily inflate byte arrays (zlib decompression)
+     */
+    private fun ByteArray.inflated():ByteArray {
+        return InflaterInputStream(this.inputStream(),Inflater()).readBytes()
     }
 }
